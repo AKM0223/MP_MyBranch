464bf5afd50b4dd129fbfd428c62970a758e5f71
Vipulbajaj-cmd <vipulbajaj77@gmail.com>
2022-05-22 17:28:32 +0530

    Added Scheduling Algorithm and Updated Jars

---
+++ adminconsole.py
@@ -0,0 +1,41 @@
+import os
+import sqlite3
+
+conn =  sqlite3.connect('services.db')
+c = conn.cursor()
+c.execute("select port_number from services")
+servicePortList = []
+rowsForPort = c.fetchall()
+for row in rowsForPort:
+    print(row[0])
+    servicePortList.append(row[0])
+print(servicePortList)
+# os.system("powershell")
+# #print("Working directory ",os.getcwd())
+contents = []
+for i in servicePortList:
+    os.system("netstat -ano | findstr :{} > port.txt".format(i))
+    with open("try.txt") as f:
+        for line in f.readlines():
+            contents.append(line)
+        f.close()
+print(contents)
+portNumber = []
+print(len(contents))
+for i in range(0,len(contents)):
+    if i%2 == 0:
+        portNumber.append(contents[i][-6:-1])
+
+print(portNumber)
+num = int(input("Enter 1 to Shut all Services"))
+if num == 1:
+    for i in portNumber:
+        os.system("taskkill /PID {} /F".format(int(i)))
+    
+# with open("try.txt") as f:
+#     contents = f.readlines()
+#     print(contents[0])
+#     port= contents[0]
+#     portNumber = port[-5:]
+#     print(portNumber)
+    
